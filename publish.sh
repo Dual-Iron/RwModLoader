533d8a8720e5127ba6058ccb07cf4f0e6a9fc36c
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2022-04-24 20:07:13 -0400

    Try new way of getting manual install

--- publish.sh
+++ publish.sh
@@ -4,4 +4,7 @@ dotnet restore
 dotnet build VirtualEnums -c Release --no-restore
 dotnet build Realm -c Release --no-restore
 dotnet publish Backend -c Release -f net6.0 -r win-x64 --self-contained -o "output" -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=link -p:TrimmerDefaultAction=link -p:EnableCompressionInSingleFile=true
-mv output/backend.exe UserInstaller.exe
+cp -a output/backend.exe UserInstaller.exe
+cp -a RwBep ManualInstall
+mkdir "ManualInstall/BepInEx/realm"
+cp -a output/backend.exe ManualInstall/BepInEx/realm/backend.exe
